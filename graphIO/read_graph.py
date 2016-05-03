9992cd8bf54363cbee00da9ce8a391d70c92af29
msionkin <msionkin@gmail.com>
2016-05-03 14:39:45 +0300

    Fix data sets reading
    Now we reads weights of edges as 'float' type values.
    Also added handle of oneline comments in data sets: if the line
    beggins with '#' sign then this line is a comment.

--- graphIO/read_graph.py
+++ graphIO/read_graph.py
@@ -5,10 +5,12 @@ def read_from_file(filename):
     graph = nx.Graph()
     with open(filename, "r") as f:
         for line in f:
+            if line[0] == '#':
+                continue
             edge_parts = line.split()
             if len(edge_parts) == 3:
                 w = edge_parts[2]
             else:
                 w = 1
-            graph.add_edge(edge_parts[0], edge_parts[1], weight=w)
+            graph.add_edge(edge_parts[0], edge_parts[1], weight=float(w))
     return graph
